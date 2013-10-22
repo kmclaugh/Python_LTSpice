5d6ccc954fdf636d1801359cc58f12c804f272d2
Kevin McLaughlin <kmclaugh@bu.edu>
2013-10-21 23:43:51 -0400

    Added error checking functionality. Shows .log in geddit if error occurs. Added .log file to raw value class

--- python_ltspice_tools.py
+++ python_ltspice_tools.py
@@ -4,14 +4,16 @@
 import re
 import os
 
+#----------------------------------------Remove Path from Name----------------------------------------#{{{1
 def remove_path_from_name(full_filename):
     """removes the path info from a given full filename"""
     base_filename = full_filename.split("/")[-1]
     return(base_filename)
+#----------------------------------------END Remove Path from Name----------------------------------------#}}}
 
-
+#----------------------------------------Parameter Statement Class----------------------------------------#{{{1
 class parameter_statement_class:
-    """a container class for carrying around parameter statement info"""
+    """A container class for carrying around parameter statement info"""
     def __init__(self,variable,value,line_number=None):
         self.variable = variable
         self.value = value
@@ -20,9 +22,9 @@ class parameter_statement_class:
         return("{}={}".format(self.variable,self.value))
     def __str__(self):
         return("{}={}".format(self.variable,self.value))
+#----------------------------------------END Parameter Statement Class----------------------------------------#}}}
 
-
-
+#----------------------------------------Parameter Check----------------------------------------#{{{1
 def parameter_check(line):
     """finds the parameter and value for a given line and parameter"""
     regex_string = "\S+ *= *\S+"
@@ -41,7 +43,9 @@ def parameter_check(line):
         return(return_list)
     else:
         return(False)
+#----------------------------------------END Parameter Check----------------------------------------#}}}
 
+#----------------------------------------Find Given Param Statement----------------------------------------#{{{1
 def find_given_param_statement(variable,line):
     """finds the parameter statement for a given line and vairable"""
     regex_string = "{} *= *\S+".format(variable)
@@ -51,7 +55,9 @@ def find_given_param_statement(variable,line):
         return(param_find[0])
     else:
         print("error finding parameter {}".format(variable))
+#----------------------------------------END Find Given Param Statemen----------------------------------------#}}}
 
+#----------------------------------------Change Parameter Value----------------------------------------#{{{1
 def change_parameter_value(a_param,new_value,current_lines):
     """changes the value of the given parameter to the given new value in the current_lines list.
         Returns the new_lines list with the parameter changed"""
@@ -62,7 +68,19 @@ def change_parameter_value(a_param,new_value,current_lines):
     new_lines = current_lines
     new_lines[a_param.line_number] = new_line
     return(new_lines)
+#----------------------------------------END Change Parameter Value----------------------------------------#}}}
 
+#----------------------------------------Create Local Log File----------------------------------------#{{{1
+def create_local_log_file(log_filename,log_lines):
+    """creates a local copy of the .log and returns the filename"""
+    new_log_filename = log_filename.split("/")[-1]
+    file = open(new_log_filename,"w")
+    for line in log_lines:
+        file.write(line+"\n")
+    return(new_log_filename)
+#----------------------------------------END Create Local Lof File----------------------------------------#}}}
+       
+#----------------------------------------Netlist Class----------------------------------------#{{{1
 class netlist_class:
     """A container class for carrying around info about a netlist file"""
     def __init__(self,linux_filename):
@@ -151,9 +169,136 @@ class netlist_class:
         self.write_file()
         command = "run_netlist.sh {}".format(self.wine_filename)
         os.system(command)
+        
+        #Read in log file and check for errors
+        print("reading in .log file")
+        log_filename = self.linux_filename.replace(".net",".log")
+        file = open(log_filename,"r")
+        log_lines = file.readlines()
+        edited_lines = []
+        error = False
+        for line in log_lines:
+            edited_line = line[:-2]
+            edited_lines.append(edited_line)
+            if "Error" in line:
+                error = True
+        log_lines = edited_lines
+        local_log_filename = create_local_log_file(log_filename,log_lines)
+        if error == True:
+            print("there was an error. See .log file")
+            command = "gedit {}&".format(local_log_filename)
+            os.system(command)
+            return(False)
 
+        else: #if no errors make raw_values object
+            print("reading in raw file")
+            raw_values = raw_values_class(self.linux_filename,log_filename=local_log_filename,log_lines=log_lines)        
+            return(raw_values)
+#----------------------------------------END Netlist Class----------------------------------------#}}}
+
+#----------------------------------------Variable Value Class----------------------------------------#{{{1
+class node_value_class:
+    """Container class for node-value pairs"""
+    def __init__(self,node,node_number,node_type,value=None):
+        self.node = node
+        self.value = value
+        self.node_number = node_number
+        self.node_type = node_type
+        self.find_unit()
+    def __repr__(self):
+        return_string = "{} = {} {}".format(self.node,self.value,self.unit)
+        return(return_string)
+    def __str__(self):
+        return_string = "{} = {} {}".format(self.node,self.value,self.unit)
+        return(return_string)
+    def find_unit(self):
+        """pulls the unit, ie V, A, Ohm, out of the node type"""
+        if "voltage" in self.node_type:
+            self.unit = "V"
+        elif "current" in self.node_type:
+            self.unit = "A"
+#----------------------------------------END Variable Value Class----------------------------------------#}}}
+
+#----------------------------------------Pull Node----------------------------------------#{{{1
+def pull_node(line):
+    """Pulls the node, node number, and node type out of the line. Returns a node_value_class obj"""
+    elements = line.split("\t")
+    elements = elements[1:]
+
+    #node number
+    node_number = int(elements[0])
+    #node name
+    node = elements[1].split("(")
+    node = node[1].replace(")","")
+    #node type
+    node_type = elements[2]
+    node_value_pair = node_value_class(node=node,node_number=node_number,node_type=node_type)
+    return(node_value_pair)
+#----------------------------------------END Pull Vairable----------------------------------------#}}}
+
+#----------------------------------------Pull Value----------------------------------------#{{{1
+def pull_value(line):
+    """pulls the float value out of a given value line and returns a float obj"""
+    float_regex = re.compile("-*\d.\d+e[-+]\d+")
+    string_value = float_regex.findall(line)[0]
+    float_value = float(string_value)
+    return(float_value)
+#----------------------------------------END Pull Value----------------------------------------#}}}
+
+#----------------------------------------Raw Values Class----------------------------------------#{{{1
+class raw_values_class:
+    """Class for a raw file output. Contains a dictionary with all the node names and their values.
+        Has methods for reading in a .raw file. Can convert a given .net filename into corresponding .raw"""
+    def __init__(self,filename,log_filename,log_lines):
+        self.raw_filename = filename.replace(".net",".raw")#convert .net to .raw
+        self.log_filename = log_filename
+        self.log_lines = log_lines
+        self.name = remove_path_from_name(self.raw_filename)
+        self.read_in_file()
+
+    def __repr__(self):
+        return(self.name)
+    def __str__(self):
+        return(self.name)
+
+    def read_in_file(self):
+        """Reads in the raw collect and collects the node and value info storing them indiciviually in a 
+            node value class. Returns a dictionary of node names and corresponding node value classes"""
+        file = open(self.raw_filename,"r")
+        self.raw_file_lines = file.readlines()
+        file.close()
+        
+        start_nodes = False
+        start_values = False
+        value_count = 0
+        node_values = []
+        for line in self.raw_file_lines:
+            line=line[:-2]
+            if "Variables:" in line:
+                if "No. Variables" not in line:
+                    start_nodes = True
+
+            elif "Values:" in line:
+                start_nodes = False
+                start_values = True
+
+            elif start_nodes == True:
+                node_value_pair = pull_node(line)
+                node_values.append(node_value_pair)
+
+            elif start_values == True:
+                value = pull_value(line)
+                node_values[value_count].value = value
+                value_count += 1
+        node_dictionary = {}
+        for node_value in node_values:
+            node_dictionary[node_value.node] = node_value
+        self.nodes_values = node_dictionary
+#----------------------------------------END Raw Values Class----------------------------------------#}}}
+        
 original_filename = "/home/kevin/.wine/drive_c/Program Files/LTC/LTspiceIV/Photoresistor_Array/Photoresistor_Array.net"
 original_netlist = netlist_class(original_filename)
 change_params = {"R00":"1k","R01":"5k"}
 new_netlist = original_netlist.change_parameters(change_params)
-new_netlist.run_netlist()
+new_raw_values = new_netlist.run_netlist()
+print(new_raw_values.nodes_values)
